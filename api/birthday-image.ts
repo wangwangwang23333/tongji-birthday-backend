50f7489e289b6df455b162fb836cb779e8dbc0e9
汪明杰 <1209497940@qq.com>
2025-12-11 20:48:48 +0800

    add code

--- api/birthday-image.ts
+++ api/birthday-image.ts
@@ -5,7 +5,6 @@ import OpenAI from "openai";
 const openai = new OpenAI({
   apiKey: 'sk-73f2f2064dc44d89b4c1e3d646b40571',
   baseURL:
-    process.env.QWEN_BASE_URL ||
     "https://dashscope.aliyuncs.com/compatible-mode/v1",
 });
 
